7f7d17947ca922cca021a1dda1cae02dd8178676
Bisma Ashar <82564303+bismaashar@users.noreply.github.com>
2022-10-24 23:06:58 +0500

    Create nightwatch.conf.js

---
+++ Unit Testing/Collaborators/nightwatch.conf.js
@@ -0,0 +1,20 @@
+module.exports = {
+    
+  src_folders: ["tests"],
+
+  webdriver: {
+    start_process: true,
+    port: 9515,
+    server_path: "node_modules/chromedriver/lib/chromedriver/chromedriver.exe",
+  },
+
+  test_settings: {
+    default: {
+      launch_url: 'https://nightwatchjs.org',
+      desiredCapabilities: {
+        browserName: "chrome",
+        javascriptEnabled: true
+    }
+    }
+  }
+};
