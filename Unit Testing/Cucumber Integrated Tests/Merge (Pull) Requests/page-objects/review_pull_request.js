c845a9215ecd443cf283825b07e8e27363fcffc1
Burak Bin Munir <108978796+burakbinmunir@users.noreply.github.com>
2022-10-29 22:49:42 +0500

    Added Page Object Model file "review pull request"
    This file contains the definition of page object models required for the step-definition of "review pull request" feature.

---
+++ Unit Testing/Cucumber Integrated Tests/Merge (Pull) Requests/page-objects/review_pull_request.js
@@ -0,0 +1,74 @@
+module.exports = {
+    url : 'https://github.com/login',
+
+    elements : {
+        login : {
+            selector: "#login_field"
+        },
+
+        password : {
+            selector: "#password"
+        },
+
+        repositoryNameField: {
+            selector : '//*[@id="dashboard-repos-filter-left"]',
+            locateStrategy: 'xpath'
+        },
+
+        repository : {
+            selector :'/html/body/div[5]/div/aside/div/loading-context/div/div[1]/div/ul/li/div/div/a',
+            locateStrategy : 'xpath'
+        },
+
+        pullRequestTab : {
+            selector : '//*[@id="pull-requests-tab"]',
+            locateStrategy : 'xpath'
+        },
+
+        pullRequest : {
+            selector : '//*[@id="issue_1_link"]',
+            locateStrategy : 'xpath'
+        },
+
+        fileChangesTab : {
+            selector : '//*[@id="repo-content-turbo-frame"]/div/div[2]/div[2]/nav/a[4]',
+            locateStrategy : 'xpath'
+        },
+
+        reviewDropDown : {
+            selector : '//*[@id="review-changes-modal"]/summary',
+            locateStrategy: 'xpath'
+        },
+
+        commentBox : {
+            selector: '//*[@id="pull_request_review_body"]',
+            locateStrategy : 'xpath'
+        },
+
+        requestChangesRadioButton : {
+           selector:  '//*[@id="review-changes-modal"]/div/div/div/form/div[3]/label/input',
+           locateStrategy : 'xpath'
+        },
+        
+        submitReviewButton: {
+            selector: '//*[@id="review-changes-modal"]/div/div/div/form/div[4]/button',
+            locateStrategy: 'xpath'
+        },
+
+        approveRequestRadioButton : {
+            selector: '//*[@id="review-changes-modal"]/div/div/div/form/div[2]/label/input',
+            locateStrategy: 'xpath'
+        },
+        
+        conversationCommentBox : {
+            selector: '//*[@id="new_comment_field"]',
+            locateStrategy: 'xpath'
+        },
+
+        commentButton : {
+            selector: '//*[@id="partial-new-comment-form-actions"]/div/div[2]/button',
+            locateStrategy: 'xpath'
+        }
+
+    }
+}
