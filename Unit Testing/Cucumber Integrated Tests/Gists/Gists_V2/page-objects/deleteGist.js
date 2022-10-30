d87758a6dfc49bab27e8a8fcecdc37b1a02724a6
Izza Mujeeb Ahmad <82564549+dark-coder12@users.noreply.github.com>
2022-10-30 22:35:28 +0500

    Create deleteGist.js

---
+++ Unit Testing/Cucumber Integrated Tests/Gists/Gists_V2/page-objects/deleteGist.js
@@ -0,0 +1,24 @@
+ module.exports = {
+
+    url: 'https://gist.github.com/dark-coder12/06c508dc6890adba0afb3b379da3eca7',
+    
+     elements: {
+
+        login : {
+            selector: "#login_field"
+        },
+        password : {
+            selector: "#password"
+        },
+
+        loginBtn: {
+            selector : "#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block.js-sign-in-button"
+        },
+
+        deleteBtn: {
+            selector : "#gist-pjax-container > div.gisthead.pagehead.pb-0.pt-3.mb-4 > div > div.mb-3.d-flex.px-3.px-md-3.px-lg-5 > ul > li:nth-child(2) > form > button"
+        }
+     },
+   
+ }
+ 
