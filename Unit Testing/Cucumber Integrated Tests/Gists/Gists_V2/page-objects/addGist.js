9e31b1f025eddfa39f24060f5619e94464cad310
Izza Mujeeb Ahmad <82564549+dark-coder12@users.noreply.github.com>
2022-11-06 01:09:45 +0500

    Update addGist.js

--- Unit Testing/Cucumber Integrated Tests/Gists/Gists_V2/page-objects/addGist.js
+++ Unit Testing/Cucumber Integrated Tests/Gists/Gists_V2/page-objects/addGist.js
@@ -31,6 +31,44 @@ module.exports = {
             locateStrategy: 'xpath',
             selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[1]/div[3]/file-attachment/blob-editor/div[2]/div/div/div[5]/div[1]/div/div/div/div[5]"
         },
+
+        showPrivacyOpts: {
+
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[2]/div/details"
+        },
+
+        publicGist: {
+
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[2]/div/details/details-menu/label[2]/div/span[1]"
+        },
+
+        privateGist: {
+
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[2]/div/details/details-menu/label[1]/div/span[2]"
+        },
+
+        indent: {
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[1]/div[2]/file-attachment/blob-editor/div[1]/div[3]/select[2]"
+        },
+
+        two: {
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[1]/div[2]/file-attachment/blob-editor/div[1]/div[3]/select[2]/optgroup/option[1]"
+        },
+
+        four: {
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[1]/div[2]/file-attachment/blob-editor/div[1]/div[3]/select[2]/optgroup/option[2]"
+        },
+
+        eight: {
+            locateStrategy: 'xpath',
+            selector: "/html/body/div[5]/div/main/div[2]/div/form/div/div[1]/div[2]/file-attachment/blob-editor/div[1]/div[3]/select[2]/optgroup/option[3]"
+        }
      }
  }
  
